fe2f765c84218b62dcd723b365546593867cc992
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-17 16:59:16 +0200

    function components

--- REACT/src/App.jsx
+++ REACT/src/App.jsx
@@ -3,9 +3,39 @@ import reactLogo from "/assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+
+function Header() {
+  return (
+      <header>
+          <nav>
+              <img src="./react-logo.png" width="40px" />
+          </nav>
+      </header>
+  )
+}
+
+function Page() {
+  return (
+      <div>
+          <Header />
+          <h1>Reasons I'm excited to learn React</h1>
+          <ol>
+              <li>It's a popular library, so I'll be 
+              able to fit in with the cool kids!</li>
+              <li>I'm more likely to get a job as a developer
+              if I know React</li>
+          </ol>
+          <footer>
+              <small>© 2021 Ziroll development. All rights reserved.</small>
+          </footer>
+      </div>
+  )
+}
+
 function App() {
   return (
     <>
+    <Page/>
       <h1>Fun facts about React</h1>
       <img src="/assets/clouds.jpg" alt="" width={"200px"} height={"200px"}/>
       <ul>
