8049bba673d7f9fe014e99a9d26bb53ad062be98
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-17 16:21:14 +0200

    adding images

--- REACT/src/App.jsx
+++ REACT/src/App.jsx
@@ -1,19 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "/assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <nav>
-        <h1>Bob's Bistro</h1>
-        <ul>
-          <li>Menu</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <h1>Fun facts about React</h1>
+      <img src="/assets/clouds.jpg" alt="" width={"200px"} height={"200px"}/>
+      <ul>
+        <li>was first released in 2013</li>
+        <li>was originaly created by Jordan walke</li>
+        <li>Has well over 100k stars on Github</li>
+      </ul>
     </>
   );
 }
