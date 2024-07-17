a622715c7491c6db10913bd5148a965157970d0c
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-17 11:36:30 +0200

    learning react the easy way

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 ReactDOM.render(<h1>Hello,everyone!</h1>,document.querySelector('#root'))
 ReactDOM.render(<ul><li>item1</li><li>item2</li><li>item3</li></ul>,document.querySelector('#root'))
+
+
+function MainContent(){
+    return(
+        <h1>I'm learning React!</h1>
+    )
+}
+ReactDOM.render(
+     <div>
+    <MainContent/>
+    </div>
+   ,
+    document.querySelector('#root')
+)
