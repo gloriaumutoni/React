bf028e3d0c58bf9633fe2376e681c5ebc1b856e5
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-17 12:02:23 +0200

    an imperative way to program

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 ReactDOM.render(<h1>Hello,everyone!</h1>,document.querySelector('#root'))
+
+// recreating the above code using vanilla js
+let elm=document.createElement('h1')
+elm.textContent='Hello everyone, This is an imperative way to program'
+elm.className='header'
+parent=document.querySelector('#root')
+parent.appendChild(elm)
+
+
 ReactDOM.render(<ul><li>item1</li><li>item2</li><li>item3</li></ul>,document.querySelector('#root'))
 
 
