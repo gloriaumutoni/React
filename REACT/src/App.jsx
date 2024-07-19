84f7f95391a1e3e9f932b0bc27d6f5ba8c74ce19
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-19 11:05:37 +0200

    React facts

--- REACT/src/App.jsx
+++ REACT/src/App.jsx
@@ -1,12 +1,13 @@
 function App() {
   return (
-    <div className="bg-red">
-      <div className="w-[40%] m-auto">
+    <div className="">
+      <div className="w-[37%] m-auto mt-28">
         <div className=" bg-[#21222A] ">
           <img src="/assets/header.png" alt="" />
         </div>
-        <div className="bg-[#282D35] py-10 bg-[url('assets/reactjs-icon2.png')]">
-          <div className="w-[90%] mx-auto  text-white">
+        <div className="bg-[#282D35] flex py-10">
+        <div className="">
+          <div className="ml-16 text-white flex-1">
             <h2 className=" text-4xl font-bold">Fun facts about React</h2>
             <ul className="list-disc list-inside marker:text-blue-300 marker:text-2xl">
               <li>Was first released in 2013</li>
@@ -18,7 +19,11 @@ function App() {
               </li>
             </ul>
           </div>
+         
         </div>
+        <img src="assets/reactjs-icon2.png" alt="" />
+        </div>
+       
       </div>
     </div>
   );
