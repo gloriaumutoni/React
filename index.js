f63296d7dd7bb92a3c9f8d20305d822b6a93f4a6
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-25 19:40:53 +0200

    function components

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-ReactDOM.render(<h1>Hello,everyone!</h1>,document.querySelector('#root'))
+ReactDOM.render(<h1>Hello,everyone!</h1>, document.querySelector("#root"));
 
 // recreating the above code using vanilla js
-let elm=document.createElement('h1')
-elm.textContent='Hello everyone, This is an imperative way to program'
-elm.className='header'
-parent=document.querySelector('#root')
-parent.appendChild(elm)
+let elm = document.createElement("h1");
+elm.textContent = "Hello everyone, This is an imperative way to program";
+elm.className = "header";
+parent = document.querySelector("#root");
+parent.appendChild(elm);
 
+ReactDOM.render(
+  <ul>
+    <li>item1</li>
+    <li>item2</li>
+    <li>item3</li>
+  </ul>,
+  document.querySelector("#root")
+);
 
-ReactDOM.render(<ul><li>item1</li><li>item2</li><li>item3</li></ul>,document.querySelector('#root'))
-
-
-function MainContent(){
-    return(
-        <h1>I'm learning React!</h1>
-    )
+function MainContent() {
+  return <h1>I'm learning React!</h1>;
 }
 ReactDOM.render(
-     <div>
-    <MainContent/>
-    </div>
-   ,
-    document.querySelector('#root')
-)
+  <div>
+    <MainContent />
+  </div>,
+  document.querySelector("#root")
+);
 
 // JSX
 
 const page = (
-    <div>
-        <h1 className="header">This is JSX</h1>
-        <p>This is a paragraph</p>
-    </div>
-)
-ReactDOM.render(
-    page,
-    document.getElementById("root")
-)
+  <div>
+    <h1 className="header">This is JSX</h1>
+    <p>This is a paragraph</p>
+  </div>
+);
+ReactDOM.render(page, document.getElementById("root"));
 
 /* 
 Challenge: 
@@ -60,5 +59,4 @@ Create a navbar in JSX:
 //     </nav>
 // )
 
-ReactDOM.render(navbar, document.getElementById("root"))
-
+ReactDOM.render(navbar, document.getElementById("root"));
